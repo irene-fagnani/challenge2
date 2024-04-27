a5c5b3a8e6cd6e09f93f958bd76c038a7472a0a2
irene-fagnani <irene.fagnani@mail.polimi.it>
2024-04-27 17:04:38 +0000

    fix some doxygen comments

--- include/MatrixClass.hpp
+++ include/MatrixClass.hpp
@@ -7,18 +7,17 @@
 #include <string>
 #include <sstream>
 namespace algebra{
-    /*
+    /**
     @brief enumerator that indicates the storage ordering
     @param row_wise indicates the storage ordering by rows
     @param column_wise indicates the sorage ordering by columns
-
     */
     enum StorageOrder{
         row_wise,
         column_wise
     };
 
-    /*
+    /**
     @brief dynamic matrix class template
     */
     template<typename T, StorageOrder S>
@@ -36,7 +35,7 @@ namespace algebra{
         std::vector<std::size_t> inner_indexes;
         std::vector<std::size_t> outer_indexes;
 
-        /*
+        /**
         @brief check if the indexes provided are inside the dimension of the matrix or not
         @param row index
         @param column index
@@ -44,7 +43,7 @@ namespace algebra{
         */
         bool in_bound(const std::size_t r,const std::size_t c) const;
 
-        /*
+        /**
         @brief compute the number of non-zero elements
         */
         void compute_nzero();
@@ -53,7 +52,7 @@ namespace algebra{
 
         friend bool operator<(const std::array<std::size_t,2> & index1, const std::array<std::size_t,2> & index2);
 
-        /*
+        /**
         @brief constructor for the Matrix class
         @param number of total columns
         @param number of total rows
@@ -63,7 +62,7 @@ namespace algebra{
             compute_nzero();
             resize_matrix(rows,cols);}
 
-        /*
+        /**
         @brief return _rows
         @return number of rows
         */
@@ -71,7 +70,7 @@ namespace algebra{
             return _rows;
         }
 
-        /*
+        /**
         @brief return _cols
         @return number of columns
         */
@@ -79,7 +78,7 @@ namespace algebra{
             return _cols;
         }
 
-        /*
+        /**
         @brief non const call operator
         @param row index
         @param column index
@@ -87,7 +86,7 @@ namespace algebra{
         */
         T & operator()(const std::size_t i,const std::size_t j);
 
-        /*
+        /**
         @brief const call operator
         @param row index
         @param column index
@@ -95,17 +94,17 @@ namespace algebra{
         */
         T operator()(const std::size_t i,const std::size_t j) const;
 
-        /*
+        /**
         @brief perform the compression, according the storage order defined at compile time
         */
         void compress();
 
-        /*
+        /**
         @brief perform the decompression, according the storage order defined at compile time
         */
         void uncompress();
 
-        /*
+        /**
         @brief allows to know if the matrix is compressed or not
         @return compressed private member of the class
         */
@@ -115,28 +114,28 @@ namespace algebra{
             compressed=value;
         }
 
-        /*
+        /**
         @brief resize a given matrix, given the numbers of column and rows
         */
         void resize_matrix(std::size_t rows, std::size_t cols);
 
-        /*
-     @brief given the name of the mtx file, the function write its content in a MatrixClass object
-     @param name of the mtx file
-     @return MatrixClass object
-     */
+        /**
+        @brief given the name of the mtx file, the function write its content in a MatrixClass object
+        @param name of the mtx file
+        @return MatrixClass object
+        */
         void read_matrix(const std::string & filename);
 
-        /*
- @brief operator* overloading for performing the multiplication between a matrix A and a vector v, A*v
- @param vector that will be multiplicated with the matrix
- @return result of the multiplication
- */
+        /**
+        @brief operator* overloading for performing the multiplication between a matrix A and a vector v, A*v
+        @param vector that will be multiplicated with the matrix
+        @return result of the multiplication
+        */
 
         std::vector<T> operator*(const std::vector<T> & v);
 
 
-        /*
+        /**
         @brief print matrix
         */
         void print_matrix();
