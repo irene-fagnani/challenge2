75700872e60b83ff81e6bc92f874bd639d156441
irene-fagnani <irene.fagnani@mail.polimi.it>
2024-05-03 06:45:04 +0000

    add private members + operator()

--- include/MatrixClass.hpp
+++ include/MatrixClass.hpp
@@ -33,6 +33,8 @@ namespace algebra{
 
     /**
      * @brief dynamic matrix class template
+     * @tparam T type of the elements in the matrix
+     * @tparam S storage order of the matrix
     */
     template<typename T, StorageOrder S>
     class MatrixClass{
@@ -57,12 +59,20 @@ namespace algebra{
          * @param column index
          * @return true if the indexes provided are inside the matrix dimension, false otherwise
         */
-        bool in_bound(const std::size_t r,const std::size_t c) const;
+        bool in_bound(const std::size_t r,const std::size_t c) const{
+            return r < _rows && c < _cols;
+        }
 
         /**
          * @brief compute the number of non-zero elements
         */
-        void compute_nzero();
+        void compute_nzero(){
+            for (auto it = _data.begin(); it != _data.end(); it++) {
+            if (it->second != T{}) {
+                _nnz++;
+            }
+        }
+        }
 
     public:
 
@@ -100,7 +110,39 @@ namespace algebra{
          * @param column index
          * @param value to add in the matrix in (i,j) position
         */
-        T & operator()(const std::size_t i,const std::size_t j);
+        T & operator()(const std::size_t i,const std::size_t j){
+            if (!in_bound(i, j)) {
+            std::cerr << "Index out of bounds." << std::endl;
+        }
+        if (!is_compressed()) {
+            if constexpr (S == StorageOrder::row_wise) {
+                return _data[{i, j}];
+            }
+
+            if constexpr (S == StorageOrder::column_wise) {
+                return _data[{j, i}];
+            }
+
+        } else {
+            if constexpr (S == StorageOrder::row_wise) {
+                for (std::size_t ii = inner_indexes[i]; ii < inner_indexes[i + 1]; ++ii) {
+                    if (outer_indexes[ii] == j) {
+                        return values[ii];
+                    }
+                }
+            } else if constexpr (S == StorageOrder::column_wise) {
+                for (std::size_t jj = outer_indexes[j]; jj < outer_indexes[j + 1]; ++jj) {
+                    if (inner_indexes[jj] == i) {
+                        return values[jj];
+                    }
+
+                }
+            }
+        }
+        std::cerr<<"In the case of compressed matrix can only change the value of existing non-zero elements.\n"<<std::endl;
+
+
+        }
 
         /**
          * @brief const call operator
@@ -108,7 +150,49 @@ namespace algebra{
          * @param column index
          * @param value to add in the matrix in (i,j) position
         */
-        T operator()(const std::size_t i,const std::size_t j) const;
+        T operator()(const std::size_t i,const std::size_t j) const{
+             if (!in_bound(i, j)) {
+            std::cerr << "Index out of bounds." << std::endl;
+        }
+        if (!is_compressed()) {
+            if constexpr (S == StorageOrder::row_wise) {
+                if (_data.find({i, j}) == _data.end()) {
+                    return 0;
+                } else {
+                    return _data.at({i, j});
+                }
+
+
+            }
+            if constexpr (S == StorageOrder::column_wise) {
+                if (_data.find({j, i}) == _data.end()) {
+                    return 0;
+                } else {
+                    return _data.at({j, i});
+                }
+
+            }
+        } else {
+            if constexpr (S == StorageOrder::row_wise) {
+                for (std::size_t ii = inner_indexes[i]; ii < inner_indexes[i + 1]; ++ii) {
+                    if (outer_indexes[ii] == j) {
+                        return values[ii];
+                    }
+
+                }
+                return 0;
+            } else if constexpr (S == StorageOrder::column_wise) {
+                for (std::size_t jj = outer_indexes[j]; jj < outer_indexes[j + 1]; ++jj) {
+                    if (inner_indexes[jj] == i) {
+                        return values[jj];
+                    }
+
+                }
+                return 0;
+            }
+        }
+
+        }
 
         /**
          * @brief perform the compression, according the storage order defined at compile time
